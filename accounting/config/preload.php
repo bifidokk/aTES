805bd2fc785e5805d65bb6120ecdc121af316253
bifidokk <bifidokk@gmail.com>
2022-05-10 19:42:01 +0300

    HW-3 Add accounting service skeleton

---
+++ accounting/config/preload.php
@@ -0,0 +1,5 @@
+<?php
+
+if (file_exists(dirname(__DIR__).'/var/cache/prod/Accounting_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/var/cache/prod/Accounting_KernelProdContainer.preload.php';
+}
